57c10d85e645386ea5e67ee7cc78d06ced396d4e
Pranav bhama <pranavbhama28@gmail.com>
2026-02-24 15:41:25 +0530

    feat: refactor banner to use String.join for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,44 @@
 /**
- * OOPSBannerApp - UC2: ASCII Art Banner
- * Displays "OOPS" in a large 7-line grid format.
+ * OOPSBannerApp - UC3: Memory Efficient Banner
+ * Uses String.join() to construct the banner lines.
  * @author Pranav
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  ***      ***    ** **     **** ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  ** **     ****  ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println("*     *  *     *  *             * ");
-        System.out.println("  ***      ***    *         ****  ");
+        // UC3: Using String.join() to manage memory efficiency
+        // Syntax: String.join("delimiter", "part1", "part2", ...)
+
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ******",
+                " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                " ******",
+                " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"));
+
+        System.out.println(String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "));
     }
 }
