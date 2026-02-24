858aad4c93f76e77e0d18455ad82253b89eb3777
Pranav bhama <pranavbhama28@gmail.com>
2026-02-24 15:27:32 +0530

    fix: align ASCII banner characters for UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
+/**
+ * OOPSBannerApp - UC2: ASCII Art Banner
+ * Displays "OOPS" in a large 7-line grid format.
+ * @author Pranav
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  ***      ***    ** **     **** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ** **     ****  ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("  ***      ***    *         ****  ");
     }
 }
